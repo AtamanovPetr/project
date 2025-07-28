4abac205be0987f6f1aa6ec5ad31639b9bbe937e
Peter <peteratamanov@yandex.ru>
2025-07-28 13:35:05 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,32 @@
+let person = {
+  name: "Peter",
+  age: "23",
+  isMarried: false,
+};
+
+console.log(person.name);
+console.log(person.age);
+
+// alert('Hello, World!')
+// let answer = confirm('Are you here?');
+
+// console.log(answer);
+
+// let answer = +prompt('Есть ли вам 18?', 'Да');
+
+// console.log(typeof(answer));
+
+let incr = 10,
+  decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+console.log('2' === 2);
+
+let isChecked = true,
+  isClosed = false;
+
+console.log(isChecked && isClosed);
+
